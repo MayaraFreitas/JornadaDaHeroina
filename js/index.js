e935fe22a472c92ee8d74e122537f9363e5d7b7e
MayaraFreitas <mah.alvesfreitas@gmail.com>
2020-05-18 20:11:28 -0300

    estrutura index quase pronta

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     getInterviews();
+    getHosts();
 });
 
-function getCardHtml(){
+function getInterviewsCardHtml(){
 
     return "<div class=\"col-xs-12 col-md-6\">"
-    + "<div class=\"box card\">"
+    + "<div class=\"box organization-card\">"
         + "<div class=\"photo\"></div>"
         + "<div class=\"episode-info\">"
             + "<div class=\"epp-number\">#_interviewNumber_</div>"
@@ -19,7 +20,7 @@ function getCardHtml(){
 }
 
 function getInterviews(){
-    var flickerAPI = "https://api.sheetson.com/v1/sheets/Entrevistas?spreadsheetId=14HNQD6hduOtPvJzUX4nOmxFp071diarN1lVlyI6iadg";
+  var flickerAPI = "https://api.sheetson.com/v1/sheets/Entrevistas?spreadsheetId=14HNQD6hduOtPvJzUX4nOmxFp071diarN1lVlyI6iadg";
   $.getJSON( flickerAPI, {
     tags: "mount rainier",
     tagmode: "any",
@@ -28,14 +29,57 @@ function getInterviews(){
     .done(function( data ) {
       $.each( data.results, function( i, item ) {
 
-        var row = getCardHtml();
+        var row = getInterviewsCardHtml();
         row = row
         .replace("_interviewNumber_", item.Index)
-        .replace("_name_", item.Name)
-        .replace("_workFunction_", item.WorkFunction)
-        .replace("_description_", item.Description)
-        .replace("_companyName_", item.CompanyName);
-        $("#cards").append(row);
+        .replace("_name_", isValid(item.Name) ? item.Name : "Anônimo")
+        .replace("_workFunction_", isValid(item.WorkFunction) ? item.WorkFunction : "Anônimo")
+        .replace("_description_", isValid(item.Description) ? item.Description : "Anônimo")
+        .replace("_companyName_", isValid(item.CompanyName) ? item.CompanyName : "Anônimo");
+        $("#interview-cards").append(row);
       });
     });
+}
+
+function getHostsCardHtml(){
+  return "<div class=\"col-xs-12 col-md-6\">"
+    + "<div class=\"box hosts-card\">"
+    + "<div class=\"photo\"><img src=\"../img/hosts/_photo_\"></div>"
+    + "<div class=\"info\">"
+    + "<div class=\"name\">_name_</div>"
+    + "<div class=\"work-function\">_function_</div>"
+    + "<div class=\"description\">_description_</div>"
+    + "</div></div></div>";
+}
+
+function getHosts(){
+  var flickerAPI = "https://api.sheetson.com/v1/sheets/Hosts?spreadsheetId=14HNQD6hduOtPvJzUX4nOmxFp071diarN1lVlyI6iadg";
+  $.getJSON( flickerAPI, {
+    tags: "mount rainier",
+    tagmode: "any",
+    format: "json"
+  })
+  .done(function( data ) {
+    $.each( data.results, function( i, item ) {
+      var row = getHostsCardHtml();
+      row = row
+      .replace("_name_", isValid(item.Name) ? item.Name : "Anônimo")
+      .replace("_function_", isValid(item.Function) ? item.Function : "Anônimo")
+      .replace("_description_", isValid(item.Description) ? item.Description : "[Sem Descrição]")
+      .replace("_photo_", isValid(item.Photo) ? item.Photo : "undefined-host.png")
+      $("#hosts-cards").append(row);
+    });
+  });
+}
+
+function isValid(item){
+  if(item == null || item == "" || item == "undefined"){
+    return false;
+  }
+  return true;
+}
+
+function scrollTo() {
+  console.log('SCROOOLLLL: ');
+  //$(id).scrollIntoView({behavior: "smooth"})
 }
